47b3dd35c82c0d57eb5c340b9b2d7795bcb960c2
casheu <huescaras@gmail.com>
2022-12-21 20:08:40 +0700

    edit layout

--- app.py
+++ app.py
@@ -9,13 +9,16 @@ import pandas as pd
 import joblib
 import yfinance as yf
 import matplotlib.pyplot as plt
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import re
+import numpy as np
+import seaborn as sns
 
 st.set_page_config(
     page_title='TESIA',
     layout='wide',
     initial_sidebar_state='expanded'
 )
-
 st.markdown('---')
 
 # Download
@@ -29,22 +32,22 @@ nlp = tf.keras.models.load_model('model_nlp')
 
 def run():
     stock = st.selectbox('Pick a stock:', ('BBNI', 'BBRI', 'BBTN', 'BMRI'))
-
     st.markdown('---')
 
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
             st.subheader('Twitter Sentiment')
+            st.markdown('---')
         with col2:
             st.subheader("Price Prediction")
+            st.markdown('---')
 
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
-            st.markdown('---')
-
             st.subheader("Today's Tweet")
+            st.markdown('---')
 
             attributes_container = []
             today = datetime.datetime.now()
@@ -63,14 +66,12 @@ def run():
             tweets['Date Created'] = tweets_daily
             tweets['Date Created'] = pd.to_datetime(tweets['Date Created']).dt.date
 
-            st.markdown('---')
-
             st.dataframe(tweets)
+            st.markdown('---')
 
         with col2:
-            st.markdown('---')
-
             st.subheader("Price History")
+            st.markdown('---')
 
             # Scrapping
             stock = yf.Ticker(stock + ".JK")
@@ -79,13 +80,91 @@ def run():
             year_rn = hist_lat['Date'].iloc[0].year
             month_rn = hist_lat['Date'].iloc[0].month
             SY_SM = hist_all[(hist_all['Date'].dt.year == year_rn) & (hist_all['Date'].dt.month == month_rn)]
-
-            st.markdown('---')
             
             fig = plt.figure(figsize=(15, 5))
             plt.plot(SY_SM['Date'], SY_SM['Close'], label = 'Actual')
             plt.legend()
             st.pyplot(fig)
+            st.markdown('---')
+
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            if st.button('Predict sentiment'):
+                st.markdown('---')
+                df = pd.DataFrame()
+        
+                # Mendefinisikan stopwords bahasa Indonesia
+                idn = list(set(stopwords.words('indonesian')))
+                factory = StemmerFactory()
+                stemmer = factory.create_stemmer()
+
+                def text_process(text):
+                    # Mengubah Teks ke Lowercase
+                    text = text.lower()
+                    
+                    # Menghilangkan Mention
+                    text = re.sub("@[A-Za-z0-9_]+", " ", text)
+                    
+                    # Menghilangkan Hashtag
+                    text = re.sub("#[A-Za-z0-9_]+", " ", text)
+                    
+                    # Menghilangkan \n
+                    text = re.sub(r"\\n", " ",text)
+                    
+                    # Menghilangkan Whitespace
+                    text = text.strip()
+
+                    # Menghilangkan Link
+                    text = re.sub(r"http\S+", " ", text)
+                    text = re.sub(r"www.\S+", " ", text)
+
+                    # Menghilangkan yang Bukan Huruf seperti Emoji, Simbol Matematika (seperti μ), dst
+                    text = re.sub("[^A-Za-z\s']", " ", text)
+
+                    # Menghilangkan RT
+                    text = re.sub("rt", " ",text)
+
+                    # Melakukan Tokenisasi
+                    tokens = word_tokenize(text)
+
+                    # Menghilangkan Stopwords
+                    text = ' '.join([word for word in tokens if word not in idn])
+
+                    # Melakukan Stemming
+                    text = ' '.join([stemmer.stem(word) for word in text.split()])
+                    
+                    return text
+
+                df['Tweet_processed'] = tweets['Tweet'].apply(lambda x: text_process(x))
+                pred = np.argmax(nlp.predict(df['Tweet_processed']), axis=-1)
+                pred_df = pd.DataFrame(pred, columns=['label'])
+                pred_df['label'] = pred_df.replace([0,1,2], ['positive','neutral','negative'])
+
+                def PieComposition(dataframe, column):
+                    palette_color = sns.color_palette('pastel')
+                    data = {}
+                    freq = {}
+                    datalen = len(dataframe[column].unique())
+                    x = np.arange(datalen)
+                    dq = dataframe[column].unique()
+                    for i in x:
+                        data[i] = dq[i]
+                        freq[i] = dataframe[column][dataframe[column] == dq[i]].value_counts().sum()
+                    data = list(data.values())
+                    freq = list(freq.values())
+                    fig = plt.figure(figsize=(10, 5))
+                    plt.pie(freq, labels = data, colors=palette_color, autopct='%.0f%%')
+                    plt.show()
+                    st.pyplot(fig)
+
+                st.subheader("Sentiment Percentage")
+                PieComposition(pred_df, 'label')
+                st.markdown('---')
+
+        with col2:
+            st.subheader("Price Prediction")
+            st.markdown('---')
 
 if __name__ == '__main__':
     run()
